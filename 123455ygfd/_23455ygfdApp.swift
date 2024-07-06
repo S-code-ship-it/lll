ad28c5b657714c144696d59190367eeed673c41d
Santosh <connectsantosh07@gmail.com>
2024-07-06 12:01:58 +0800

    Initial Commit

---
+++ 123455ygfd/_23455ygfdApp.swift
@@ -0,0 +1,17 @@
+//
+//  _23455ygfdApp.swift
+//  123455ygfd
+//
+//  Created by T Krobot on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct _23455ygfdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
